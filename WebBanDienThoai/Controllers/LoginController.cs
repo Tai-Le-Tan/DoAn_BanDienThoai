6bc0f5d48a728df6b84fc3b773cd5c3a29fa61fc
NamLa <nam.darkstar@gmail.com>
2021-06-10 18:30:12 +0700

    update

---
+++ WebBanDienThoai/Controllers/LoginController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanDienThoai.Models;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace WebBanDienThoai.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        private WEBBANDIENTHOAIEntities db = new WEBBANDIENTHOAIEntities();
+        public ActionResult LoginAccount()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginAccount(string email, string password)
+        {
+
+            var loginKh = db.Customers.SingleOrDefault(x => x.Email == email && x.Password == password && x.Status == true);
+            //Md5Encode md5 = new Md5Encode();
+            //var passmd5 = md5.EncodeMd5Encrypt(password);
+            var login = db.Users.SingleOrDefault(x => x.Email == email && x.Password == password && x.Status == true);
+
+            if (login != null)
+            {
+
+
+                Session["UserID"] = login.UserID;
+                Session["Username"] = login.Username;
+                Session["Email"] = login.Email;
+                Session["Password"] = login.Password;
+                Session["Image"] = login.Image;
+                return Redirect("~/AdminHome/Index");
+
+
+            }
+            else
+
+            if (loginKh != null)
+            {
+                Session["customerID"] = loginKh.customerID;
+                Session["customerName"] = loginKh.customerName;
+                Session["Email"] = loginKh.Email;
+                Session["Password"] = loginKh.Password;
+
+                return Redirect("~/Home/Index");
+            }
+
+            else
+            {
+
+                ViewBag.error = "Tên Tài Khoản Hoặc Mật Khẩu Không Đúng";
+            }
+
+            return View();
+        }
+        public ActionResult RegisterUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RegisterUser(Customer _user)
+        {
+            if (ModelState.IsValid)
+            {
+                var check_id = db.Customers.Where(s => s.Email == _user.Email).FirstOrDefault();
+                if (check_id == null)
+                {
+                    if (_user.Password == _user.ComfirmPass)
+                    {
+
+                        _user.CreatedAt = DateTime.Now;
+                        _user.Status = true;
+                        db.Configuration.ValidateOnSaveEnabled = false;
+
+                        db.Customers.Add(_user);
+                        db.SaveChanges();
+                        return RedirectToAction("LoginAccount");
+                    }
+                    else
+                    {
+                        ViewBag.ErrorRegister1 = "Sai mật khẩu ";
+                    }
+                }
+                else
+                {
+                    ViewBag.ErrorRegister = "This Email is exist";
+                    return View();
+
+                }
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult ForgotPass(string email)
+        {
+            Customer cus = db.Customers.Where(s => s.Email == email).FirstOrDefault();
+            #region Send mail
+
+
+            MimeMessage message = new MimeMessage();
+
+            MailboxAddress from = new MailboxAddress("Admin",
+            "lanam.9907704@gmail.com");
+            message.From.Add(from);
+
+            MailboxAddress to = new MailboxAddress("User", cus.Email);
+            message.To.Add(to);
+
+            message.Subject = "Reset Mật khẩu thành công";
+            BodyBuilder bodyBuilder = new BodyBuilder();
+            bodyBuilder.HtmlBody = $"<h1>Mật khẩu của bạn là: {cus.Password}  </h1>";
+            bodyBuilder.TextBody = "Mật Khẩu của bạn";
+            message.Body = bodyBuilder.ToMessageBody();
+            // xac thuc email
+            SmtpClient client = new SmtpClient();
+            //connect (smtp address, port , true)
+            client.Connect("smtp.gmail.com", 465, true);
+            client.Authenticate("lanam.990704@gmail.com", "qceonpbfjcgpdvoa");
+            //send email
+            client.Send(message);
+            client.Disconnect(true);
+            client.Dispose();
+            #endregion
+            return RedirectToAction("LoginAccount");
+
+        }
+
+
+    }
+}
---
+++ WebBanDienThoai/Controllers/UsersController.cs
@@ -0,0 +1,134 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanDienThoai.Models;
+
+namespace WebBanDienThoai.Controllers
+{
+    public class UsersController : Controller
+    {
+        private WEBBANDIENTHOAIEntities db = new WEBBANDIENTHOAIEntities();
+        // GET: Users
+        public ActionResult Index(int? page, string q)
+        {
+            var count_user = (from cus in db.Users select cus.UserID).Count();
+
+            ViewBag.count_user = count_user;
+
+            var model = from p in db.Users orderby p.CreatedAt descending select p;
+            int pagesize = 15;
+            int pageNumber = (page ?? 1);
+
+            if (!string.IsNullOrEmpty(q))
+            {
+                model = model.Where(x => x.Username.Contains(q) || x.Email.Contains(q) || x.Fullname.Contains(q)).OrderByDescending(x => x.CreatedAt);
+            }
+
+            ViewBag.keyword_search = q;
+
+            return View(model.ToPagedList(pageNumber, pagesize));
+        }
+
+
+
+
+        // GET: Admin/Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(User user, HttpPostedFileBase image_avatar, bool status_mi)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                user.Password = user.Password;
+                user.CreatedAt = DateTime.Now;
+
+                bool tus;
+                if (status_mi == true)
+                {
+                    tus = true;
+                }
+                else
+                {
+                    tus = false;
+                }
+
+                var checkemail = db.Users.Count(x => x.Email == user.Email);
+                if (checkemail > 0)
+                {
+                    ViewBag.erroremail = "Email Đăng Kí Đã Tồn Tại";
+                }
+                else
+                {
+                    if (image_avatar != null)
+                    {
+                        var filename = Path.GetFileName(image_avatar.FileName);
+                        var path = Path.Combine(Server.MapPath("~/Upload/Images"), filename);
+
+
+                        image_avatar.SaveAs(path);
+                        user.Image = "/Upload/Images/" + image_avatar.FileName;
+                    }
+                    else
+                    {
+                        user.Image = "/Upload/Default/man-avatar.jpg";
+                    }
+                    user.Status = tus;
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+            }
+
+            return View(user);
+        }
+
+
+
+
+
+
+        // GET: Admin/Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+
+            User user = db.Users.Find(id);
+
+            if (user.Image != null)
+            {
+                var filename = Path.GetFileName(user.Image);
+                System.IO.File.Delete(Request.PhysicalApplicationPath + "/Upload/Images/" + filename);
+            }
+
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
